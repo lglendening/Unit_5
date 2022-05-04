9b5d91bd1ec9f5d29f61c9dac22c2d833a064fc9
Logan G <lpglendening25@students.bmchs.org>
2022-05-04 10:08:47 -0500

    pushed to git

---
+++ DoNow5.04.py
@@ -0,0 +1,31 @@
+'''
+###############
+# Do Now 5.04 #
+###############
+
+1. In your Console
+------------------
+Type the following:
+my_dictionary = {
+'a': 1,
+'b': 2,
+'c': 3,
+'d': 4,
+'e': 5
+}
+print(list(my_dictionary))
+
+In your Notebook
+----------------
+Respond to the following:
+Write down what list() does when given a dictionary.
+
+What type does list() return?
+
+Write down how you might use a for loop to go through and print the values of my_dictionary.
+
+2. In your Console
+------------------
+Challenge yourself
+Try your solution to 3.
+'''
