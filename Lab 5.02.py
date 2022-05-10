a72ba901f3f545165661e2ce1bc4acaf7e15c3a9
Logan G <lpglendening25@students.bmchs.org>
2022-05-10 08:53:19 -0500

    done with lab

--- Lab 5.02.py
+++ Lab 5.02.py
@@ -48,3 +48,51 @@ example_paragraph_lower_no_punctuation = example_paragraph_lower.replace(".", ""
 #convert paragraph into a list of individual strings
 example_word_list = example_paragraph_lower_no_punctuation.split(" ")
 '''
+
+
+standard_paragraph = (
+                    "The man walked into the house. He sits down in the recliner , picks up the remote and" 
+                    "turns on the TV , The TV boots up to the local news station . The news reporter says "
+                    "' Good afternoon everyone today a money at the zoo has ' and the guy changes the channel "
+                    "to Nick Jr. and the pink bubble guppie says ' eaten his lunch ! ' Then the guy turns off "
+                    "the TV and goes to make himself a plate of nachos . He grabs the cheese and chips and "
+                    "throws all the ingredients together as fast as possible . He sits down with his mega"
+                    "plate of nachos and watches a show while sipping Dr . Pepper ."
+                    )
+
+
+
+def frequency(word):
+    if word in standard_paragraph:
+        print(f"The word {word} is in the list.")
+    else:
+        print(f"The word {word} isnt in the list.")
+
+
+#make all letters lowercase
+example_paragraph_lower = standard_paragraph.lower()
+
+#remove all periods
+example_paragraph_lower_no_punctuation = example_paragraph_lower.replace(".", "")
+                       
+#convert paragraph into a list of individual strings
+example_word_list = example_paragraph_lower_no_punctuation.split(" ")
+
+while True:
+
+    what_the_user_want_do = input("What would you like to do? [see if word in list (1), make all lowercase (2), remove all periods(3), "
+                                    "or make all words different lists (4)]   ")
+
+    if what_the_user_want_do == "1":
+        word_to_check = input("What word do you wanna check? ")
+        frequency(word_to_check)
+    elif what_the_user_want_do == "2":
+        paragraph_lower = standard_paragraph.lower()
+        print(paragraph_lower)
+    elif what_the_user_want_do == "3":
+        paragraph_lower_no_punctuation = example_paragraph_lower.replace(".", "")
+        print(paragraph_lower_no_punctuation)
+    elif what_the_user_want_do == "4":
+        word_list = example_paragraph_lower_no_punctuation.split(" ")
+        print(word_list)
+ 
